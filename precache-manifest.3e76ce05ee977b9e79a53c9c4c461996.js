ad7eec34983cbc3ab0c8c610274a741a52e79b72
kimgyver <jinyoung777@gmail.com>
2019-07-25 15:29:11 +1200

    Updates

---
+++ precache-manifest.3e76ce05ee977b9e79a53c9c4c461996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38acb5e40bf8fc5f3c56a91556e2273",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a3ca24b4ac1cdbd24002",
+    "url": "/monsters-rolodex/static/css/main.e3832273.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a3ca24b4ac1cdbd24002",
+    "url": "/monsters-rolodex/static/js/main.3c174abc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
